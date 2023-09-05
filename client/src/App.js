794a5ea46364bc485e7ff90d66a9ed1a1c30a5f0
shameen-hub <shameenshahid19@gmail.com>
2023-09-05 16:30:07 +0500

    first

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h3> Hi this is my first ever AWS pipeline creation</h3>
+      <p>
+        {" "}
+        Let's explore different steps in which we can create a CI/CD pipeline
+      </p>
+    </div>
+  );
+}
+
+export default App;
